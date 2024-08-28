c3dd8d9d36859145531ec0bb50d18ac710ff2ed4
LongTypeNo6 <longtypeno.6@gmail.com>
2024-08-28 19:26:30 +0900

    검색기능 구현중5
    품목검색되는데 페이지처리가 엉망임

--- src/main/generated/site/junggam/procurement_system/mapper/ContractMapperImpl.java
+++ src/main/generated/site/junggam/procurement_system/mapper/ContractMapperImpl.java
@@ -8,7 +8,7 @@ import site.junggam.procurement_system.entity.Contract;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-28T15:37:21+0900",
+    date = "2024-08-28T19:25:23+0900",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
